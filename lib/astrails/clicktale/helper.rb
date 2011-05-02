67e35a99328c9fd0cc50fcf5951a439ef55aec5d
Michael Mazyar <michael@astrails.com>
2011-05-02 11:23:09 +0300

    add specific html format for partial rendering, otherwise in some edge cases using formats other than html rails fails to find the partial

--- lib/astrails/clicktale/helper.rb
+++ lib/astrails/clicktale/helper.rb
@@ -3,13 +3,13 @@ module Astrails
     module Helper
       def clicktale_top
         return unless clicktale_enabled?
-        render :partial => "clicktale/top"
+        render :partial => "clicktale/top.html.erb"
       end
 
       def clicktale_bottom
         return unless clicktale_enabled?
 
-        render :partial => "clicktale/bottom", :locals => {
+        render :partial => "clicktale/bottom.html.erb", :locals => {
           :project_id => clicktale_config[:project_id],
           :path => clicktale_url,
           :ratio => clicktale_config[:ratio] || 1,
